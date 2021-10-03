63b3049b07feb16a000c6624096e163ea200b38a
codemeleon <anmol.kiran@gmail.com>
2021-10-03 23:38:51 +0200

    Removal of ruffus

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from sys import version
 from setuptools import setup
 
 setup(
-    name="BackFill",
-    version="0.1",
-    scripts=["ab1_integration.py"],
+    name="SeqPatcher",
+    version="0.0.1",
+    scripts=["seqpatcher.py"],
     install_requires=[
         "pandas==1.2.4",
         "biopython==1.78",
         "click==7.1.2",
-        "ruffus==2.8.4",
     ],
     # packages=[],
     # package_dir={},
-    url="https://github.com/krisp-kwazulu-natal/sars-cov-2-sequencing-merge-sanger",
+    url="https://github.com/krisp-kwazulu-natal/seqPatcher",
     license="GPLv3",
     author="Anmol Kiran; San James Emmanuel",
     author_email="anmol.kiran@gmail.com;sanemmanueljames@gmail.com",
